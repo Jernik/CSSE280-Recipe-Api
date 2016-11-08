444f2d4db242acc3a395f29bfb203154395967e6
Jernik <Luke.Miller.DRSS@gmail.com>
2016-11-08 16:28:30 -0500

    second register fix

--- routes/register.js
+++ routes/register.js
@@ -4,22 +4,31 @@ var router = express.Router();
 
 
 router.route('/').post(function (req, res, next) { // CONSIDER: can add a next parameter for next middleware to run in the middleware chain
-    console.log(req.cookies);
     var Profile = mongoose.model("Profile");
-    console.log(req.body);
     var newProfile = new Profile({
         username: req.body.username,
-        passwordHash: req.body.passwordHash// Need to add the rest of the fields
+        passwordHash: req.body.passwordHash,
+        email:req.body.email,
+        firstName:req.body.firstName,
+        lastName:req.body.lastName
     });
-    //TODO make sure this username is unique
-    newProfile.save(function (err, contact) {
-        if (err) {
-            res.send('Problem registering.');
-        } else {
-            res.cookie('login', contact._id); //go ahead and log the user in too
-            res.send("registered");
+    Profile.count({username: req.body.username}, function (err, count) {
+        if (count === 0) {
+            newProfile.save(function (err, contact) {
+                if (err) {
+                    res.send('Problem registering.');
+                } else {
+                    res.cookie('login', contact._id); //go ahead and log the user in too
+                    res.send("registered");
+                }
+            });
+        } else{
+            res.status(409);
+            res.body = res.body + "username already in use";
+            res.send();
         }
-    });
+    })
+
 });
 
 module.exports = router;
